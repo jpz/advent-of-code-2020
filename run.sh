93c81c7846682d209dff4953a2a49b2f84ec83cd
Jason Zavaglia <jason.zavaglia@gmail.com>
2020-12-06 17:39:15 +1100

    add run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# get full path of current directory
+# (source: https://stackoverflow.com/a/246128)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+for file in "$DIR/build/bin/"*
+do
+    echo "$(basename ${file}):"
+    (cd "${DIR}/data"; "$file")
+    echo "" #-----------"; echo ""
+done
